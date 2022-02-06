a1b4e480e2246d124b836f86b7309a5974a59236
arman <armanthaque@gmail.com>
2022-02-06 06:24:47 -0500

    First Test Cases

--- app/src/test/java/com/example/logindatabase/ExampleUnitTest.java
+++ app/src/test/java/com/example/logindatabase/ExampleUnitTest.java
@@ -10,8 +10,16 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+//     UserList: 1 - armanthaque@yahoo.com, zarrin622
+//     UserList: 2 - zarrinshaque@gmail.com, arman622
+//     UserList: 3 - ariHaque@gmail.com, haque622
+//     UserList: 5 - Armanthaque@gmail.com, abc213
+//     UserList: 8 - bobbuilder@gmail.com, password123
+//
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+    
 }
